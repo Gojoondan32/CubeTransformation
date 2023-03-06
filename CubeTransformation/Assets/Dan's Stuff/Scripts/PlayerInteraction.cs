7a469af0814d4022d327fd139bd3157d3e825ab8
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2023-03-06 14:55:54 +0000

    Rays now detect the grid

---
+++ CubeTransformation/Assets/Dan's Stuff/Scripts/PlayerInteraction.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Oculus.Interaction;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    [SerializeField] private RayInteractor rayInteractor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
