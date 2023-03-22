c0fd3225784e2f2fb6430bde8a69de29ab03d515
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2023-03-22 10:40:37 +0000

    Remove MonoBehaviour from Current Position

--- CubeTransformation/Assets/Conrad's Stuff/PathWayTesting/Chambers/CurrentPosition.cs
+++ CubeTransformation/Assets/Conrad's Stuff/PathWayTesting/Chambers/CurrentPosition.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CurrentPosition : MonoBehaviour
+public class CurrentPosition
 {
     public static int CurrentDimension;
     public static int CurrentChamber;
