ca7638700008de4ddfe450f7b78755794591eeec
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2023-03-22 10:39:17 +0000

    CurrentDImension & Chamber

---
+++ CubeTransformation/Assets/Conrad's Stuff/PathWayTesting/Chambers/CurrentPosition.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurrentPosition : MonoBehaviour
+{
+    public static int CurrentDimension;
+    public static int CurrentChamber;
+}
