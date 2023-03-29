e780b180cde668a3c898ff0dd54e827ef4b4412f
CodeSmither <75310607+CodeSmither@users.noreply.github.com>
2023-03-29 12:21:23 +0100

    Added Finish Level

---
+++ CubeTransformation/Assets/Conrad's Stuff/PathWayTesting/LevelFinish.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFinish : MonoBehaviour
+{
+    [SerializeField]
+    private WorldTransition worldTransition;
+    [SerializeField]
+    private int CurrentWorld;
+    [SerializeField]
+    private GameObject TeleParticle;
+
+    private void Awake()
+    {
+        TeleParticle.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        LevelComplete();
+    }
+
+    private void LevelComplete()
+    {
+        
+        if (CurrentPosition.CurrentChamber == 3 && LevelTimer.ChambersComplete[CurrentWorld] == true)
+        {
+            StartCoroutine(Teleport());
+            
+        }
+        
+        
+    }
+
+    private IEnumerator Teleport()
+    {
+        if (TeleParticle != null)
+        {
+            TeleParticle.SetActive(true);
+            yield return new WaitForSeconds(2f);
+            TeleParticle.SetActive(false);
+        }
+       worldTransition.Loadscene(0);
+
+    }
+}
